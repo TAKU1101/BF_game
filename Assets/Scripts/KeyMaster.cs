d2bd847288c7d038058e0e723d3b968933cfd059
TAKU1101 <taku.kuma1101@icloud.com>
2021-04-17 22:21:21 +0900

    Add: input

---
+++ Assets/Scripts/KeyMaster.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Text.RegularExpressions;
+
+public class KeyMaster : MonoBehaviour
+{
+    public InputField playerInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerInput.Select();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string text = playerInput.text;
+        playerInput.Select();
+        string parse = delSet(text, "+-[]<>.,");
+        playerInput.text = parse;
+        Debug.Log(parse);
+    }
+    
+    private string delSet(string str, string set)
+    {
+        string  res = "";
+
+        MatchCollection matche = Regex.Matches(str, @"[\-\[\]+.,<>]*");
+        foreach (Match m in matche)
+        {
+            res += m.Value;
+        }
+        return (res);
+    }
+}
---
+++ Assets/Scripts/StackMaster.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StackMaster : MonoBehaviour
+{
+    public Text counterText;
+    public int counter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counterText.text = counter.ToString();
+    }
+}
